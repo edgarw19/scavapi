23837bfc0069bffbcabaddf64b486e4ad7f4ae14
edgarw19 <edgar@dispatch.me>
2017-08-01 00:47:51 -0400

    update

--- api/controllers/BroadcastController.js
+++ api/controllers/BroadcastController.js
@@ -25,68 +25,17 @@ exports.send_a_broadcast = function (req, res) {
     var bMessage = req.body.message;
     //get Accesstoken
     request({
-        url: authUri,
+        url: "https://graph.facebook.com/v2.6/me/messages?access_token=EAAbCDZBtoZCGQBAJ93PEGrZBFAeBsvfHhcdoqwJ3f4o6JH9CcP8yQQqE61tqiUK6UEyEZBnSoCf7vXwDbIGy81y0CIOA8tejdGcl5qU0RFAKJcwusXZAD1iJOOYqSjC7xeZCuOh7FAPk2R8ys3MnZBOniMdI8Ai99UbeDFTsOmz1gZDZD",
         method: "POST",
         headers: {
-            'Host': 'login.microsoftonline.com',
-            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/json'
         },
         form: {
-            grant_type: 'client_credentials',
-            client_id: clientId,
-            client_secret: clientSecret,
-            scope: scope
+            message: bMessage
         },
     }, function (error, response, body) {
         if (response && (response.statusCode === 200 || response.statusCode === 201)) {
-            var obj = JSON.parse(response.body);
-            if (obj != null) {
-                var accesstoken = obj.access_token;
-                if (accesstoken != "") {
-                    //Go through all users with subscribed feature.
-                    Users.find({ isSubscribed: true }, function (err, user) {
-                        for (var i = 0; i < user.length; i++) {
-                            var userObj = user[i];
-                            console.log(broadcastUri + userObj.fbMessengerId + "-" + botAppName + "/activities");
-                            request({
-                                url: broadcastUri + userObj.fbMessengerId + "-" + botAppName + "/activities",
-                                method: "POST",
-                                json: true,
-                                headers: {
-                                    'Authorization': 'Bearer ' + accesstoken,
-                                    'Content-Type': 'application/json'
-                                },
-                                body: {
-                                    "type": "message",
-                                    "from": {
-                                        "id": botFBId,
-                                        "name": botFBName
-                                    },
-                                    "conversation": {
-                                        "id": userObj.fbMessengerId + "-" + botAppName,
-                                        "name": "Convo1"
-                                    },
-                                    "recipient": {
-                                        "id": userObj.fbMessengerId,
-                                        "name": userObj.firstName + " " + userObj.lastName
-                                    },
-                                    "text": bMessage,
-                                    "replyToId": botFBId
-                                }
-                            }, function (error1, response1, body1) {
-                                console.log(response);
-                                if (response1 && (response1.statusCode === 200 || response1.statusCode === 201)) {
-                                    //broadcast sent
-                                    if ((user.length - 1) == i) {
-                                        res.json(1);
-                                    }
-                                }
-                            });
-                        }
-                    });
-                }
-                console.log(accesstoken);
-            }
+            console.log("SUCCESS")
         }
     });
     res.json(1);
