61d8cbdf7a398055c75820c6d1f3182dd2a0230c
edgarw19 <edgar@dispatch.me>
2017-08-01 00:51:17 -0400

    update stuff

--- api/controllers/BroadcastController.js
+++ api/controllers/BroadcastController.js
@@ -24,19 +24,26 @@ var mongoose = require('mongoose'),
 exports.send_a_broadcast = function (req, res) {
     var bMessage = req.body.message;
     //get Accesstoken
-    request({
-        url: "https://graph.facebook.com/v2.6/me/messages?access_token=EAAbCDZBtoZCGQBAJ93PEGrZBFAeBsvfHhcdoqwJ3f4o6JH9CcP8yQQqE61tqiUK6UEyEZBnSoCf7vXwDbIGy81y0CIOA8tejdGcl5qU0RFAKJcwusXZAD1iJOOYqSjC7xeZCuOh7FAPk2R8ys3MnZBOniMdI8Ai99UbeDFTsOmz1gZDZD",
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        form: {
-            message: bMessage
-        },
-    }, function (error, response, body) {
-        if (response && (response.statusCode === 200 || response.statusCode === 201)) {
-            console.log("SUCCESS")
+    Users.find({ isSubscribed: true }, function (err, user) {
+        for (var i = 0; i < user.length; i++) {
+            var userObj = user[i];
+            request({
+                url: "https://graph.facebook.com/v2.6/me/messages?access_token=EAAbCDZBtoZCGQBAJ93PEGrZBFAeBsvfHhcdoqwJ3f4o6JH9CcP8yQQqE61tqiUK6UEyEZBnSoCf7vXwDbIGy81y0CIOA8tejdGcl5qU0RFAKJcwusXZAD1iJOOYqSjC7xeZCuOh7FAPk2R8ys3MnZBOniMdI8Ai99UbeDFTsOmz1gZDZD",
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                form: {
+                    recipient: {"id": userObj.fbMessengerId},
+                    message: {"text": bMessage}
+                },
+            }, function (error, response, body) {
+                if (response && (response.statusCode === 200 || response.statusCode === 201)) {
+                    console.log("SUCCESS")
+                }
+            });
+
         }
-    });
+    })  
     res.json(1);
 };
